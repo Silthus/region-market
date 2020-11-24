b3fc45f7a18e1244879293d0033d1cca104bf82c
Silthus <michael@reichenbach.in>
2020-11-24 23:26:33 +0100

    fix(limits): apply default limit to all players
    #8

--- src/main/java/net/silthus/regions/limits/LimitsConfig.java
+++ src/main/java/net/silthus/regions/limits/LimitsConfig.java
@@ -1,6 +1,7 @@
 package net.silthus.regions.limits;
 
 import de.exlll.configlib.annotation.Comment;
+import de.exlll.configlib.annotation.ElementType;
 import de.exlll.configlib.annotation.Ignore;
 import de.exlll.configlib.configs.yaml.BukkitYamlConfiguration;
 import de.exlll.configlib.format.FieldNameFormatters;
@@ -21,6 +22,11 @@ public class LimitsConfig extends BukkitYamlConfiguration {
     private final RegionsPlugin plugin;
 
     @Comment({
+            "The default limit is automatically applied to all players regardless of their permissions.",
+            "Set the option to a blank string to disable the default limit."
+    })
+    private String defaultLimit = "default";
+    @Comment({
             "Define your limits in this config.",
             "Each limit has a unique key that can be assigned to players by giving them the rcregions.limits.<your-limit> permission.",
             "You can overwrite specific limits per player with the following permissions:",
@@ -28,6 +34,7 @@ public class LimitsConfig extends BukkitYamlConfiguration {
             "   - rcregions.player-limits.regions.<group>.<limit>",
             "   - rcregions.player-limits.groups.<limit>"
     })
+    @ElementType(Limit.class)
     public Map<String, Limit> limits = new HashMap<>();
 
     public LimitsConfig(RegionsPlugin plugin, Path path) {
@@ -48,6 +55,7 @@ public class LimitsConfig extends BukkitYamlConfiguration {
                 .filter(permission -> permission.startsWith(Constants.LIMITS_PREFIX))
                 .map(s -> s.replace(Constants.LIMITS_PREFIX, ""))
                 .map(s -> limits.get(s))
+                .map(limit -> limits.get(defaultLimit))
                 .filter(Objects::nonNull)
                 .reduce(Limit::combine)
                 .map(limit -> new PlayerLimit(player, limit))
