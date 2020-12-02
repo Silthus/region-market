25213842064759e6238eb2335fb146019b21f642
Michael Reichenbach <michael.reichenbach@interhyp.de>
2020-12-02 14:00:46 +0100

    fix(db): fix pending drops migration of owner_id on regions table

--- src/main/resources/dbmigration/mysql/1.3.sql
+++ src/main/resources/dbmigration/mysql/1.3.sql
@@ -1,9 +1,3 @@
 -- apply changes
-alter table rcregions_regions add column owner_id varchar(40);
-
 alter table rcregions_region_groups add column world varchar(255);
 alter table rcregions_region_groups add column world_guard_region varchar(255);
-
-create index ix_rcregions_regions_owner_id on rcregions_regions (owner_id);
-alter table rcregions_regions add constraint fk_rcregions_regions_owner_id foreign key (owner_id) references rcregions_players (id) on delete restrict on update restrict;
-
--- src/main/resources/dbmigration/sqlite/1.3.sql
+++ src/main/resources/dbmigration/sqlite/1.3.sql
@@ -1,8 +1,3 @@
 -- apply changes
-alter table rcregions_regions add column owner_id varchar(40);
-
 alter table rcregions_region_groups add column world varchar(255);
 alter table rcregions_region_groups add column world_guard_region varchar(255);
-
-create index ix_rcregions_regions_owner_id on rcregions_regions (owner_id);
-
