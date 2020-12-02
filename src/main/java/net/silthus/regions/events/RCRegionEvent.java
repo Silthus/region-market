4d8985d290e57128a95b4a7d1c57062f35774315
Silthus <michael@reichenbach.in>
2020-12-02 18:25:10 +0100

    fix(api): add static handlerlist to base events

--- src/main/java/net/silthus/regions/events/RCRegionEvent.java
+++ src/main/java/net/silthus/regions/events/RCRegionEvent.java
@@ -1,8 +1,11 @@
 package net.silthus.regions.events;
 
+import lombok.Data;
 import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 
+@Data
 public abstract class RCRegionEvent extends Event {
 
-
+    private static final HandlerList handlerList = new HandlerList();
 }
--- src/main/java/net/silthus/regions/events/RegionEvent.java
+++ src/main/java/net/silthus/regions/events/RegionEvent.java
@@ -3,10 +3,13 @@ package net.silthus.regions.events;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.silthus.regions.entities.Region;
+import org.bukkit.event.HandlerList;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public abstract class RegionEvent extends RCRegionEvent {
+
+    private static final HandlerList handlerList = new HandlerList();
 
     private final Region region;
 
